864b48655b7817adaee355d738f8b4c0076cabe6
Sinan Bolel <1915925+sbolel@users.noreply.github.com>
2021-11-13 03:41:34 -0500

    build: add eslint and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,58 @@
+module.exports = {
+  env: {
+    es6: true,
+    node: true,
+  },
+  extends: [
+    'plugin:jsdoc/recommended',
+    'plugin:prettier/recommended',
+    'prettier',
+  ],
+  rules: {
+    'jsdoc/newline-after-description': 'off',
+    'jsdoc/require-jsdoc': ['warn', { publicOnly: true }],
+    'no-prototype-builtins': 'warn',
+    'no-restricted-globals': ['error', 'name', 'length'],
+    'no-useless-escape': 'warn',
+    'prefer-arrow-callback': 'error',
+    'prefer-promise-reject-errors': 'warn',
+    'require-atomic-updates': 'off',
+    'require-jsdoc': 'warn',
+    'valid-jsdoc': 'warn',
+    'prettier/prettier': [
+      'error',
+      {
+        arrowParens: 'always',
+        printWidth: 100,
+        semi: false,
+        singleQuote: true,
+        tabWidth: 2,
+        trailingComma: 'es5',
+      },
+    ],
+  },
+  globals: {},
+  overrides: [
+    {
+      files: ['*.spec.*'],
+      env: {
+        jest: true,
+      },
+    },
+  ],
+  parserOptions: {
+    parser: 'babel-eslint',
+    sourceType: 'module',
+  },
+  plugins: [
+    'jsdoc',
+    'prettier',
+  ],
+  settings: {
+    jsdoc: {
+      tagNamePreference: {
+        returns: 'return',
+      },
+    },
+  },
+}
